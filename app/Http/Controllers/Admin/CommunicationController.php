1dc191d3871d6708d52fb2b3f1193c08488fe018
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-10-15 13:55:20 +0300

    Update CommunicationController.php

--- app/Http/Controllers/Admin/CommunicationController.php
+++ app/Http/Controllers/Admin/CommunicationController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;  
  
 use App\User;
-use Illuminate\Http\Request;   
+use Illuminate\Http\Request;    
 use Illuminate\Support\Facades\Log; 
 use App\Http\Controllers\Controller;
 use Brian2694\Toastr\Facades\Toastr;
