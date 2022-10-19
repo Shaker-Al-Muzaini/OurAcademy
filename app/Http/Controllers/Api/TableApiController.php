0f6fd2e78acd2aded06eea8a54cd005c78f507c4
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-10-19 09:56:21 +0300

    Update TableApiController.php

--- app/Http/Controllers/Api/TableApiController.php
+++ app/Http/Controllers/Api/TableApiController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
   
-use App\AboutPage;
+use App\AboutPage; 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
