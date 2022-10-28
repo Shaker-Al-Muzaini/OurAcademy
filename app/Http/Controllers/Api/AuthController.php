f93c5d898fcc3b7d5aff18c60e7ff7e55c1862eb
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-10-28 17:47:10 +0200

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,7 +1,7 @@
 <?php
    
 namespace App\Http\Controllers\Api;       
-       
+        
 use App\Http\Controllers\Auth\LoginController;            
 use App\Http\Controllers\Controller;       
 use App\Models\Language;  
