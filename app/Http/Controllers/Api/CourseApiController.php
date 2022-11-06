8fc3ce0d8c208a90e3df75fd9368547d59e14862
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-11-06 18:36:38 +0200

    Update CourseApiController.php

--- app/Http/Controllers/Api/CourseApiController.php
+++ app/Http/Controllers/Api/CourseApiController.php
@@ -1,6 +1,6 @@
 <?php
  
-namespace App\Http\Controllers\Api;  
+namespace App\Http\Controllers\Api;   
        
 use Illuminate\Http\Request;        
 use Illuminate\Routing\Controller;                
